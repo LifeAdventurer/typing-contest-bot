816377446ad3924aeeb07e9272075c5a00c214f6
lifeadventurer <life0adventurer@gmail.com>
2024-09-11 15:54:40 +0800

    Refactor: Change bot setup to class-based structure

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import asyncio
 import json
 
 import discord
 from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 def load_token() -> str:
@@ -13,9 +10,34 @@ def load_token() -> str:
         return json.load(file)["token"]
 
 
-@bot.command(name="ping")
-async def ping(ctx):
-    await ctx.reply("pong")
+class TypingContestBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="start")
+    async def start(self, ctx):
+        await ctx.reply("The typing contest has started!")
+
+    @commands.command(name="end")
+    async def end(self, ctx):
+        await ctx.reply("The typing contest has ended!")
 
 
-bot.run(load_token())
+class BotSetup:
+    def __init__(self, token):
+        self.token = token
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.bot = commands.Bot(command_prefix="!", intents=self.intents)
+
+    async def setup(self):
+        await self.bot.add_cog(TypingContestBot(self.bot))
+
+    async def run(self):
+        await self.setup()
+        await self.bot.start(self.token)
+
+
+if __name__ == "__main__":
+    bot_instance = BotSetup(load_token())
+    asyncio.run(bot_instance.run())
